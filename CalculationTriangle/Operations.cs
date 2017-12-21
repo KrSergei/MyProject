c81997d9f99581af26cfe82bd5c1d210f9dfbcd7
KrSergei <NemO-S@yandex.ru>
2017-12-21 13:43:58 +0300

    Initial check-in of module CalculationTriangle

---
+++ CalculationTriangle/Operations.cs
@@ -0,0 +1,45 @@
+﻿using System;
+namespace CalculationTriangle
+{
+    public class Operations
+    {
+        public static float CalculationPerimetr(float a, float b, float c, ref p)
+        {
+           
+                if (Check(a, b, c) == false)
+                    return 0;
+                float p = a + b + c;
+                float sum = (float)Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+             //   Console.WriteLine("Периметр - {0} \tПлощадь - {1:2}", p, sum);
+                return p;
+        }
+        public static float CalculationSum(float a, float b, float c, ref sum)
+        {
+
+            if (Check(a, b, c) == false)
+                return 0;
+            float sum = (float)Math.Sqrt(CalculationPerimetr.p * (p - a) * (p - b) * (p - c));
+            //   Console.WriteLine("Периметр - {0} \tПлощадь - {1:2}", p, sum);
+            return sum;
+        }
+
+
+        private static bool Check(float a, float b, float c)
+        {
+            if (a > 0 && b > 0 && c > 0 && c < Math.Sqrt(a * a) + (b - b))
+                return true;
+            else
+            //Console.WriteLine("Can't great triangle");
+            return false;
+            
+        }
+        public static float Calculation(float a)
+        {
+            float p = 3*a;
+            float sum = (float)Math.Sqrt(p * (p - a) * (p - a) * (p - a));
+      //      Console.WriteLine("Периметр - {0} \tПлощадь - {1:2}", p, sum);
+            return sum;
+        }
+    }
+}
+
---
+++ CalculationTriangle/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace CalculationTriangle
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int type;
+            Console.WriteLine("Enter type triangle: 1 (versatile) or 2 (equilateral)");
+            type = int.Parse(Console.ReadLine());
+            if (type == 1)
+            {
+
+                Console.WriteLine("Enter the first side");
+                float x = float.Parse(Console.ReadLine());
+                Console.WriteLine("Enter the second side");
+                float y = float.Parse(Console.ReadLine());
+                Console.WriteLine("Enter the third side");
+                float z = float.Parse(Console.ReadLine());
+
+            }
+            else if (type == 2)
+            {
+                float calc;
+                Console.WriteLine("Enter leght side");
+                float x = float.Parse(Console.ReadLine());
+                calc = Operations.Calculation(x);
+            }
+            else
+            {
+                Console.WriteLine("Choise 1 or 2");
+            }
+            float calc;
+            calc = Operations.CalculationPerimetr(ref p);
+            Console.WriteLine("Leght perimetr: ", p);
+        }
+    }
+}
